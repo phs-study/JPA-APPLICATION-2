9f9057c1e2e6e508256767aa66a05910a2ddf890
lhm7877 <vkfdldh7>
2022-01-03 02:30:25 +0900

    feat: 1:N 조회 정리

--- hoomin/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ hoomin/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -27,6 +27,11 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
+    public List<Order> findAll() {
+        return em.createQuery("select o from Order o", Order.class)
+            .getResultList();
+    }
+
     public List<Order> findAllByString(OrderSearch orderSearch) {
 
         String jpql = "select o from Order o join o.member m";
@@ -81,6 +86,24 @@ public class OrderRepository {
                 " from Order o" +
                 " join o.member m" +
                 " join o.delivery d", OrderSimpleQueryDto.class)
+            .getResultList();
+    }
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+            "select distinct o from Order o" +
+                " join fetch o.member m" +
+                " join fetch o.delivery d" +
+                " join fetch o.orderItems oi" +
+                " join fetch oi.item i", Order.class)
+            .getResultList();
+    }
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+            "select o from Order o" +
+                " join fetch o.member m" +
+                " join fetch o.delivery d", Order.class)
+            .setFirstResult(offset)
+            .setMaxResults(limit)
             .getResultList();
     }
 }
